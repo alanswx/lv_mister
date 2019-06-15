7ef9f7789ded305101cea13794adad1c5d0c78c3
Alan Steremberg <alan.steremberg@gmail.com>
2019-06-15 14:37:47 -0700

    added Back

--- browser.c
+++ browser.c
@@ -93,10 +93,35 @@ printf("group_focus_cb4 page: %x\n",page);
 }
 
 lv_obj_t *gwin;
+char gorigpath[4096];
 char gpath[4096];
 static lv_res_t list_release_action(lv_obj_t * list_btn)
 {
     printf("List element click:%s\n", lv_list_get_btn_text(list_btn));
+
+
+    if (!strcasecmp("Back",lv_list_get_btn_text(list_btn))) {
+    	lv_obj_t * page = lv_win_get_content(gwin);
+    	if (page) {
+                lv_page_clean(page);
+    	}
+
+	printf("path[%s]\n",gpath);
+	if (!strcasecmp(gorigpath,gpath)) {
+		printf("at top of browser\n");
+	}	
+        // we probably want a real stack instead of using the paths..
+	char *slash = strrchr(gpath,'/');
+	if (slash) {
+		*slash=0;
+	}
+	printf("path[%s]\n",gpath);
+    	create_rombrowser_list( gwin, gpath);
+
+    	return LV_RES_OK;
+    }
+
+
     strcat(gpath,"/");
     strcat(gpath,lv_list_get_btn_text(list_btn));
     lv_obj_t * page = lv_win_get_content(gwin);
@@ -114,7 +139,10 @@ static lv_res_t list_release_action(lv_obj_t * list_btn)
 void create_rombrowser_list( lv_obj_t *win, const char  * path)
 {
 gwin=win;
-if (gpath!=path) strcat(gpath,path);
+if (gpath!=path) {
+	strcpy(gorigpath,path);
+	strcpy(gpath,path);
+}
 static lv_group_t * g;
     g = lv_group_create();
     lv_group_set_focus_cb(g, group_focus_cb);
@@ -161,7 +189,7 @@ lv_list_set_style(list, LV_LIST_STYLE_BTN_PR, &style_btn_pr);
     lv_obj_set_size(list, lv_page_get_scrl_width(lv_win_get_content(win)), lv_obj_get_height(lv_win_get_content(win)) );
     lv_group_add_obj(g, list);
 
-    lv_list_add(list, SYMBOL_FILE, "BACK", list_release_action);
+    lv_list_add(list, SYMBOL_LEFT, "Back", list_release_action);
 
 
     lv_obj_t * list_btn;
